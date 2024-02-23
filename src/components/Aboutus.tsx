d17dbffdd6ee4117761e150dde605db35a8c77ac
Tusho7 <sandrotushurashvili7@gmail.com>
2024-02-24 00:41:06 +0400

    feat: create aboutus component

---
+++ src/components/Aboutus.tsx
@@ -0,0 +1,16 @@
+const Aboutus = () => {
+  return (
+    <div className="text-white p-5 flex flex-col gap-4">
+      <h1 className="text-4xl">Why us</h1>
+      <p className="text-lg leading-[30px]">
+        At CodeNovex, we're passionate about leveraging technology to drive
+        innovation and empower businesses. With expertise in custom software
+        development, web design, and mobile app development, we deliver tailored
+        digital solutions that exceed expectations. Collaborate with us to turn
+        your ideas into reality and unlock your business's full potential.
+      </p>
+    </div>
+  );
+};
+
+export default Aboutus;
