4f153144753f47fa05a57b44aa43adf51d57dfd0
Tusho7 <sandrotushurashvili7@gmail.com>
2024-02-26 14:19:57 +0400

    feat: add aboutus component on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import { Footer } from "../components/Footer";
+import Aboutus from "../components/Aboutus";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Aboutus />
       <Footer />
     </div>
   );
