bde9f5dfcfc735ff98259f5f0f0e5e1ab7c12c30
Tusho7 <sandrotushurashvili7@gmail.com>
2024-02-27 15:02:19 +0400

    style: change icons in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Logo from "../assets/logo.png";
-import FbIcon from "../assets/facebook_icon.png";
-import LinkedinIcon from "../assets/linkedin_icon.png";
-import InstagramIcon from "../assets/instagram_icon.png";
-
+import FbIcon from "../assets/fb_icon.svg";
+import LinkedinIcon from "../assets/linkedin_icon.svg";
+import InstagramIcon from "../assets/instagram_icon.svg";
 
 export const Footer = React.forwardRef<HTMLDivElement>((_, ref) => {
   return (
-    <div className="bg-white p-12 flex flex-col gap-4 items-center mt-28" ref={ref}>
+    <div
+      className="bg-white p-12 flex flex-col gap-4 items-center mt-28"
+      ref={ref}
+    >
       <img src={Logo} alt="Logo" />
-      <p className="text-center text-base">
+      <p className="text-center text-xl w-[310px]">
         Innovate, Code, Elevate: Crafting Tomorrow's Solutions Today.
       </p>
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-5 items-center">
         <img src={FbIcon} alt="" className="w-10 h-10" />
         <img src={LinkedinIcon} alt="" className="w-10 h-10" />
         <img src={InstagramIcon} alt="" className="w-10 h-10" />
