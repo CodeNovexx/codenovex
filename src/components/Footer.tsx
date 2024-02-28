c115347b2d7d225cdf089d06b2c8338f086f4dd9
Tusho7 <sandrotushurashvili7@gmail.com>
2024-02-28 10:58:45 +0400

    feat: add phone number in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,14 +10,21 @@ export const Footer = React.forwardRef<HTMLDivElement>((_, ref) => {
       className="bg-white py-12 flex flex-col gap-4 items-center mt-28 rounded-lg md:mt-0"
       ref={ref}
     >
-      <img src={Logo} alt="Logo" className="w-36 h-36 self-center md:w-40 md:h-40" />
+      <img
+        src={Logo}
+        alt="Logo"
+        className="w-36 h-36 self-center md:w-40 md:h-40"
+      />
       <p className="text-center text-xl md:text-2xl md:w-[600px]">
         Innovate, Code, Elevate: Crafting Tomorrow's Solutions Today.
       </p>
 
-      <div className="text-xl font-sans text-gray-700 mb-7 md:text-2xl">
+      <div className="flex flex-col gap-3 text-xl font-sans text-gray-700 mb-7 md:text-2xl">
         <a href="mailto: info@codenovex.ge">
           Email: <span className="font-bold">info@codenovex.ge</span>
+        </a>
+        <a href="tel:555 05 00 01">
+          Phone: <span className="font-bold"> 555 05 00 01</span>
         </a>
       </div>
 
@@ -26,10 +33,18 @@ export const Footer = React.forwardRef<HTMLDivElement>((_, ref) => {
           <img src={FbIcon} alt="" className="w-10 h-10 md:w-12 md:h-12" />
         </a>
         <a href="" target="_blank">
-          <img src={LinkedinIcon} alt="" className="w-10 h-10 md:w-12 md:h-12" />
+          <img
+            src={LinkedinIcon}
+            alt=""
+            className="w-10 h-10 md:w-12 md:h-12"
+          />
         </a>
         <a href="" target="_blank">
-          <img src={InstagramIcon} alt="" className="w-10 h-10 md:w-12 md:h-12" />
+          <img
+            src={InstagramIcon}
+            alt=""
+            className="w-10 h-10 md:w-12 md:h-12"
+          />
         </a>
       </div>
     </div>
