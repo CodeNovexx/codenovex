72dcf703cc4d51b70693c32ab1abb87df3696964
Tusho7 <sandrotushurashvili7@gmail.com>
2025-11-03 16:00:01 +0400

    feat: Integrate ContactForm component into Home page and update footer reference

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import Header from "../components/Header";
 import { Footer } from "../components/Footer";
+import { ContactForm } from "../components/ContactForm";
 import Aboutus from "../components/Aboutus";
 import Team from "../components/Team";
 import Services from "../components/Services";
@@ -9,6 +10,7 @@ const Home = () => {
   const aboutRef = useRef<HTMLDivElement>(null);
   const teamRef = useRef<HTMLDivElement>(null);
   const servicesRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
   const footerRef = useRef<HTMLDivElement>(null);
 
   return (
@@ -17,13 +19,14 @@ const Home = () => {
         aboutRef={aboutRef}
         teamRef={teamRef}
         servicesRef={servicesRef}
-        footerRef={footerRef}
+        footerRef={contactRef}
       />
       <main className="max-w-[1440px] mx-auto">
-        <Aboutus ref={aboutRef} footerRef={footerRef} />
+        <Aboutus ref={aboutRef} footerRef={contactRef} />
         <Team ref={teamRef} />
         <Services ref={servicesRef} />
       </main>
+      <ContactForm ref={contactRef} />
       <Footer ref={footerRef} />
     </>
   );
